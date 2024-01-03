de41ceaf611f85c1f8e2d4fd6a84afaedfe64314
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-01-03 15:25:57 +0100

    user model

---
+++ models/User.php
@@ -0,0 +1,41 @@
+<?php
+
+class User
+{
+    private $id;
+    public $username;
+    private $password;
+
+    function __construct()
+    {
+
+    }
+
+    function select_users(){
+        global $db;
+        $result= $db->query("SELECT * FROM user_alpha;");
+        return $result->fetch_all(1);
+    }
+
+    function register($username, $email, $pwd)
+    {
+        global $db;
+
+        return $db->query("INSERT INTO user_alpha (name_user, email_user, password_user) VALUES ('$username', '$email', '$pwd');");
+    }
+
+    function login($email, $password)
+    {
+        global $db;
+
+        //les points sont la liason entre string email et adresse email et password et pwd
+        $result = $db->query('SELECT * FROM user_alpha WHERE email_user ="' . $email . '"  and password_user = "' . $password . '";');
+
+        return $result->fetch_assoc();
+    }
+
+    function logout()
+    {
+        session_destroy();
+    }
+}
