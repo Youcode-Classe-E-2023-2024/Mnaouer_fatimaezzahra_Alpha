57be93ef3a0d084455cc4bb28d8dce1b69258446
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-01-03 15:26:34 +0100

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+include_once 'config.php';
+
+// si la pagina est vuota mandami alla pagina di casa si no mandami alla pagina domandata
+if (isset($_GET['page'])){
+    include_once 'controllers/' . $_GET['page'] . '_controller.php';
+    include_once 'views/' . $_GET['page'] . '_view.php';
+} else {
+    include_once 'controllers/home_controller.php';
+    include_once 'views/home_view.php';
+}
